d427d576fae0038906bc7b1732b92ea868590d35
azarialii <145339205+azarialii@users.noreply.github.com>
2023-09-25 12:58:27 +0200

    Update KontoTests.cs

--- TestBank/KontoTests.cs
+++ TestBank/KontoTests.cs
@@ -22,5 +22,24 @@ namespace BankTests
             Assert.AreEqual(17, k.Guthaben);
 
         }
+          public void Auszahlen_NichtausreichenderGuthaben()
+         {
+      Konto k = new Konto(2200);
+      Assert.ThrowsException<ArgumentOutOfRangeException>(() => k.Auszahlen(3000), "Guthaben nicht ausreichend");
+
+
+      }
+      public void Auszahlen_ausreichenderGuthaben()
+      {
+      int a = 2000;
+      int b = 100;
+      Konto k = new Konto(a);
+      k.Auszahlen(b);
+
+      // Assert
+      int expectedBalance = a - b;
+      Assert.AreEqual(expectedBalance, k.Guthaben);
+
+
     }
 }
